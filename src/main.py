d97296cf196a40c6a4278a2e87f8b41b645b4aa8
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-06-08 11:15:56 +0200

    Reading
    Co-authored-by: Grosu Cristian <cristi2019255@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -4,18 +4,30 @@ from pyspark.ml.evaluation import ClusteringEvaluator
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.feature import StandardScaler
 import matplotlib.pyplot as plt
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 DATASET_PATH = "data/imdbProfiles.csv"
 
 
-def main():    
+def main():
     # starting Sprak session
     spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
     sc = spark.sparkContext
 
     # Reading the data from the csv
-    df = spark.read.csv(DATASET_PATH, header=True,inferSchema=True)
+    schema = StructType([
+        StructField("id", StringType(), False),
+        StructField("title", StringType(), True),
+        StructField("starring", StringType(), True),
+        StructField("writer", StringType(), True),
+        StructField("editor", StringType(), True)
+    ])
+
+    df = spark.read.csv(DATASET_PATH, header=True,
+                        sep='|', schema=schema)
     df.printSchema()
+
+    df.summary().show()
 
 
 if __name__ == '__main__':
