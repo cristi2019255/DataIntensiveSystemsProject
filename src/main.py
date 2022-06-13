afd9926bd5ecd22651c265684f8a7946c0d2fcf4
cristi2019255 <cristi.grosu.2013@mail.ru>
2022-06-13 11:00:57 +0200

    greedy heuristic try1

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+from numpy import maximum
 from pandas import DataFrame
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
@@ -25,7 +26,6 @@ CLUSTER_COLUMN = "cluster"
 LEFT_COLUMN = "_1"
 RIGHT_COLUMN = "_2"
 L_COLUMN = "_l"
-
 
 
 def create_session():
@@ -104,17 +104,79 @@ def clusters_homogeneity(df:DataFrame, assignments, homogeneity):
 
     print("Clustered homogenity:")
     print(homogeneity_sum / CLUSTER_COUNT)
+
+def evaluate_partition(homogeneity, partition):
+    homogeneity_sum = 0
+    for i in range(len(partition)):
+        homo = homogeneity(partition[i])
+        print(f"Part {i}, homogenity: {homo}")
+        homogeneity_sum += homo
+    
+    avg = homogeneity_sum/len(partition)
+    print(f"Avg per partition:{avg}")
+
+
+def greedy_heuristic(df:DataFrame, k: int, homogenity):    
+    df = df.drop(ID_COLUMN)
+    df_left = df.select('*')
+    
+    columns_counts = [df.groupby(c).count().sort(F.col("count"), ascending=False).collect() for c in df.columns]
+    
+    parts = []
+    for _ in range(k):
+        maximum = max([max(x, key= lambda y:y[1]) for x in columns_counts], key=lambda x:x[1])
+        
+        max_attr_val = maximum[0]
+        col = list(maximum.asDict().keys())[0]
+        
+        
+        parts.append(df.where(df[col] == max_attr_val))            
+        df_left = df_left.where(df[col] != max_attr_val)         
+        
+        columns_counts = [df_left.groupby(c).count().sort(F.col("count"), ascending=False).collect() for c in df.columns]
+        
+        parts[-1].show()
     
     
+    while df_left.count() != 0:
+        # until partition is not complete for each part add the subparts the increases homogenity the most
+        
+        # getting the maximum homogen subpart over column
+        maximum = max([max(x, key= lambda y:y[1]) for x in columns_counts], key=lambda x:x[1])        
+        max_attr_val = maximum[0]
+        col = list(maximum.asDict().keys())[0]
+
+        subpart = df.where(df[col] == max_attr_val)
+        # searching the part that increases homogenity the most with the subpart
+        best_part = 0
+        homogenity_best = 0
+        for i in range(k):
+            homo = homogenity(parts[i].union(subpart))
+            if homo > homogenity_best:
+                homogenity_best = homo
+                best_part = i 
+                
+        # merging subpart with the part
+        parts[best_part] = parts[best_part].union(subpart)
+        parts[best_part].show()
+        # removing the subpart from the dataframe
+        df_left = df_left.where(df[col] != max_attr_val)        
+        # removing the subpart from column_counts        
+        columns_counts = [df_left.groupby(c).count().sort(F.col("count"), ascending=False).collect() for c in df.columns]
+        
+        print(df_left.count())
+    
+    return parts
+        
     
 def main():
     spark, sc = create_session()
 
     df = read_data(spark)
     
-    train_clusterer(df, sc) # run it only once to train the model and then comment and just load the model for faster experiments
+    #train_clusterer(df, sc) # run it only once to train the model and then comment and just load the model for faster experiments
     
-    assignments = load_clusterer(sc).assignments().toDF().toDF(ID_COLUMN, CLUSTER_COLUMN)        
+    #assignments = load_clusterer(sc).assignments().toDF().toDF(ID_COLUMN, CLUSTER_COLUMN)        
 
     distinct = df.agg(*(F.countDistinct(F.col(column)).alias(column)
                              for column in df.columns))
@@ -122,13 +184,15 @@ def main():
     N_counts = distinct.collect()[0]
 
     homogeneity_func = generateEntropyColumnHomogenity(N_counts) #HOMOGENEITY
-    
+
+    #clusters_homogeneity(df, assignments, homogeneity=homogeneity_func)
+
+    partition = greedy_heuristic(df.drop('editor'), CLUSTER_COUNT, homogeneity_func)
 
     print("Total homogenity:")    
     print(homogeneity_func(df.drop(ID_COLUMN, CLUSTER_COLUMN)))    
 
-
-    clusters_homogeneity(df, assignments, homogeneity=homogeneity_func)
+    evaluate_partition(partition=partition, homogeneity=homogeneity_func)
 
 if __name__ == '__main__':
     main()
