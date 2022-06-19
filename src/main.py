018b97e901a88eb7211f07da74d154ad6379acd5
cristi2019255 <cristi.grosu.2013@mail.ru>
2022-06-19 13:41:54 +0200

    Updates

--- src/main.py
+++ src/main.py
@@ -8,10 +8,13 @@ from utilities import create_session, read_data
 def main():
     spark, sc = create_session()
     
-    dataset_path = "data/dunkin_stores.csv"
+    #dataset_path, separator = "data/dbpediaProfiles.csv", "|"
+    dataset_path, separator = "data/dunkin_stores.csv", ","
+    #dataset_path, separator = "data/syntheticFinances.csv", ","
+    
     dataset_name = dataset_path.split("/")[-1]
     
-    df = read_data(spark, limit=None, separator=',', path= dataset_path)    
+    df = read_data(spark, limit=None, separator=separator, path= dataset_path)    
     
     #example1(df, sc)
         
